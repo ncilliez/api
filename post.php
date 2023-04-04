2757d2ebaf12da7af03b5004731bfade8d291333
ncilliez <ncilliez@gmail.com>
2023-04-04 12:50:55 +0200

    requete post

---
+++ post.php
@@ -0,0 +1,19 @@
+<?php
+
+    $url = 'http://localhost/api/produits';
+    $data = array('name' => 'PEC', 'description' => 'Pencil 2H', 'price' => 2.25, 'category' => '9');
+
+    $options = array(
+        'http' => array(
+            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method' => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    var_dump($result);
+
+?>
--- produits.php
+++ produits.php
@@ -12,11 +12,15 @@
             }else{
                 getProducts();
             }
-            break;
+        break;
+
+        case 'POST':
+            AddProduct();
+        break;
         
         default:
             header("HTTP/1.0 405 Method Not Allowed");
-            break;
+        break;
     }
 
     function getProducts($id = null){
@@ -34,5 +38,35 @@
         header('Content-Type: application/json');
         echo json_encode($response, JSON_PRETTY_PRINT);
     }
+
+    function AddProduct(){
+        global $conn;
+        $name = $_POST["name"];
+        $description = $_POST["description"];
+        $price = $_POST["price"];
+        $category = $_POST["category"];
+        $created = date('Y-m-d H:i:s');
+        $modified = date('Y-m-d H:i:s');
+    
+        $query = "INSERT INTO produit(name, description, price, category_id, created, modified) 
+        VALUES('".$name."', '".$description."', '".$price."', '".$category."', '".$created."', '".$modified."')";
+
+        if(mysqli_query($conn, $query)) {   
+            $response=array(
+                'status'=> 1,
+                'status_message'=>'Produit ajouté avec succès.'
+            );
+        } else {
+            $response=array(
+                'status'=> 0,
+                'status_message'=>'ERREUR! '
+            );
+        }
+    
+        // Retournez la réponse en tant que JSON
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+    
 
 ?>
